01ce45f9729774485f40db4932537369e544b77c
Lisa Chen <58052077+l13a@users.noreply.github.com>
2021-09-18 18:35:22 -0400

    API stuff

---
+++ Interio/Assets/Scripts/SendAPI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendAPI : MonoBehaviour
+{
+
+    //send the majority category of furniture in the person's shopping cart
+    public string sendInfo()
+    {
+        //string[] objects = ObjectChooser.GetComponent<ObjectChooser>().avaliableObjects;
+        
+
+        return "";
+    }
+}
